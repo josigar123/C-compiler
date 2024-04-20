48a208e0071fd788e8ee7b5fdb9ba88f5a963acf
j253805 <253805@usn.no>
2024-04-20 17:01:05 +0200

    Endring i main for å parse enkel testfil

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,19 +1,24 @@
-use regex::Regex;
+use std::vec;
 
-mod lex;
+mod gen;
+mod lex_refactor;
+mod parser;
+mod token;
 
+// Loggfør ish 4 timer for lexer refaktorering søndag 7 april 12-16
 fn main() {
-    // No unrecognized output = good
-    let file_to_tokenize = "tests/lexer_tests/file_to_tokenize.c";
+    // Lexing
+    let return_int = "tests/parser_tests/return_int.c";
 
-    // No unrecognized output = good
-    let recognize_test = "tests/lexer_tests/recognize_test.txt";
+    let mut lexemes: Vec<String> = vec![];
+    let mut tokens: Vec<token::Token> = vec![];
 
-    // No output = good
-    let unrecognize_test = "tests/lexer_tests/unrecognized_test.txt";
-    let mut lexemes: Vec<String> = Vec::new();
-    lexemes = lex::get_lexemes(recognize_test);
+    lexemes = lex_refactor::get_lexemes(&return_int);
+    tokens = lex_refactor::tokenize_lexemes(lexemes);
 
-    let mut tokens: Vec<lex::Token> = Vec::new();
-    tokens = lex::tokenize_lexemes(lexemes);
+    let mut parser = magic_parsing::Parser::new(tokens);
+
+    let program_node = parser.parse_program();
+    program_node.unwrap().walk_and_print();
+    println!("Program successfully parsed!");
 }
